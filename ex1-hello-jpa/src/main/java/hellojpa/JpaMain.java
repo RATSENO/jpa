6d578f2b9f1c30f994a7a82a265e4dd71746b0d0
ratseno <onestar1014@gmail.com>
2020-01-07 00:22:05 +0900

    jpql 사용

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -20,8 +21,15 @@ public class JpaMain {
             member.setId(2L);
             member.setName("HelloB");
             */
-            Member findMember = em.find(Member.class, 1L);
-            findMember.setName("HelloJPA");
+            List<Member> result = em
+                    .createQuery("select m from Member as m", Member.class)
+                    .setFirstResult(1)
+                    .setMaxResults(10)
+                    .getResultList();
+
+            for (Member member : result){
+                System.out.println(member.getName());
+            }
 
             tx.commit();
         } catch (Exception e) {
