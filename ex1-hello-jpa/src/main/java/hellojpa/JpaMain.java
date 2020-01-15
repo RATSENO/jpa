97cbbe0e9b69ae1d850012bcda84e0dbd79eae09
ratseno <onestar1014@gmail.com>
2020-01-15 17:40:23 +0900

    테스트

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
+
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
 
         EntityManager em = emf.createEntityManager();
