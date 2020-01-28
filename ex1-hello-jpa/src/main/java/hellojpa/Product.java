83b6076ae8aa52856c297d9b58ef92cb6be5a22e
RATSENO <onestar1014@gmail.com>
2020-01-28 14:53:28 +0900

    다대다 양방향

--- ex1-hello-jpa/src/main/java/hellojpa/Product.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Product.java
@@ -3,6 +3,9 @@ package hellojpa;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Product {
@@ -12,4 +15,7 @@ public class Product {
     private Long id;
 
     private String name;
+
+    @ManyToMany(mappedBy = "products")
+    private List<Member> members = new ArrayList<>();
 }
