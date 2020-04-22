8471955bc7633ad3e5ae81da2a116034ea3baff5
ratseno <onestar1014@gmail.com>
2020-04-22 22:12:58 +0900

    test

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,9 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import java.time.LocalDateTime;
 import java.util.Iterator;
 import java.util.List;
@@ -20,38 +23,13 @@ public class JpaMain {
         tx.begin();
 
         try {
+            CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
+            CriteriaQuery<Member> query = criteriaBuilder.createQuery(Member.class);
 
-            Member member = new Member();
-            member.setUsername("user1");
-            member.setHomeAddress(new Address("homecity", "street1", "zipcode"));
+            Root<Member> m = query.from(Member.class);
 
-            member.getFavoriteFoods().add("치킨");
-            member.getFavoriteFoods().add("족발");
-            member.getFavoriteFoods().add("피자");
-
-            member.getAddressHistory().add(new Address("old1","old1","old1"));
-            member.getAddressHistory().add(new Address("old2","old2","old2"));
-
-            em.persist(member);
-
-            em.flush();
-            em.clear();
-
-            System.out.println("===================================");
-            Member findMember = em.find(Member.class, member.getId());
-            System.out.println("===================================");
-
-            //homecity >>> newcity
-            Address homeAddress = findMember.getHomeAddress();
-            findMember.setHomeAddress(new Address("newcity", homeAddress.getStreet(), homeAddress.getZipCode()));
-
-            //치킨 >>>> 한식
-            findMember.getFavoriteFoods().remove("치킨");
-            findMember.getFavoriteFoods().add("한식");
-
-            //old1 >>>> new1
-            findMember.getAddressHistory().remove(new Address("old1","old1","old1"));
-            findMember.getAddressHistory().add(new Address("new1","old1","old1"));
+            CriteriaQuery<Member> cq = query.select(m).where(criteriaBuilder.equal(m.get("username"), "kim"));
+            List<Member> resultList = em.createQuery(cq).getResultList();
 
             tx.commit();
         } catch (Exception e) {
