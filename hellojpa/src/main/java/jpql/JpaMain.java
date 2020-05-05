9c1112c094b04e54b9cb3c7836ad727a7dc7c65e
ratseno <onestar1014@gmail.com>
2020-05-05 14:09:50 +0900

    jpql - 결과조회 API - getResultList();

--- hellojpa/src/main/java/jpql/JpaMain.java
+++ hellojpa/src/main/java/jpql/JpaMain.java
@@ -21,13 +21,21 @@ public class JpaMain {
             Member member = new Member();
             member.setUsername("member1");
             member.setAge(10);
+
             em.persist(member);
 
-            //TypeQuery : 반환 타입이 명확할 때 사용
-            TypedQuery<Member> query1 = em.createQuery("select m from Member m", Member.class);
-            TypedQuery<String> query2 = em.createQuery("select m.username from Member m", String.class);
-            //반환 타입이 명확하지 않을 떄 사용
-            Query query3 = em.createQuery("select m.username, m.age from Member m");
+            Member member2 = new Member();
+            member2.setUsername("member2");
+            member2.setAge(10);
+
+            em.persist(member2);
+
+            TypedQuery<Member> query = em.createQuery("select m from Member m", Member.class);
+            List<Member> resultList = query.getResultList();
+            for (Member member1 : resultList) {
+                System.out.println(member1);
+            }
+
 
             em.flush();
 
